d73f05dcd72b68155b78a16dd59f68fdc394ce8e
rodrigomartins808 <martins.rodrigo@icloud.com>
2026-08-19 18:05:35 +0100

    fix: regenerate white logo to preserve the solid red box and white M

--- process_logo.py
+++ process_logo.py
@@ -8,47 +8,66 @@ def process_logo():
     pixels = new_img.load()
     orig_pixels = img.load()
     
-    min_x, max_x = width, 0
-    min_y, max_y = height, 0
+    red_min_x, red_max_x = width, 0
+    red_min_y, red_max_y = height, 0
     
     for y in range(height):
         for x in range(width):
             r, g, b, a = orig_pixels[x, y]
+            if r > 150 and g < 100 and b < 100:
+                red_min_x = min(red_min_x, x)
+                red_max_x = max(red_max_x, x)
+                red_min_y = min(red_min_y, y)
+                red_max_y = max(red_max_y, y)
+                
+    red_min_x -= 2
+    red_max_x += 2
+    red_min_y -= 2
+    red_max_y += 2
+
+    content_min_x, content_max_x = width, 0
+    content_min_y, content_max_y = height, 0
+
+    for y in range(height):
+        for x in range(width):
+            r, g, b, a = orig_pixels[x, y]
             
-            # Detect background: mostly white or very light gray
-            if r > 240 and g > 240 and b > 240:
-                pixels[x, y] = (255, 255, 255, 0)
-            # Detect the red M
-            elif r > 120 and g < 100 and b < 100:
-                pixels[x, y] = (r, g, b, a)
-                min_x, max_x = min(min_x, x), max(max_x, x)
-                min_y, max_y = min(min_y, y), max(max_y, y)
-            # Detect black/dark text
-            elif r < 120 and g < 120 and b < 120:
-                pixels[x, y] = (255, 255, 255, a)
-                min_x, max_x = min(min_x, x), max(max_x, x)
-                min_y, max_y = min(min_y, y), max(max_y, y)
+            in_red_box = (red_min_x <= x <= red_max_x) and (red_min_y <= y <= red_max_y)
+            
+            if in_red_box:
+                pixels[x, y] = orig_pixels[x, y]
+                content_min_x = min(content_min_x, x)
+                content_max_x = max(content_max_x, x)
+                content_min_y = min(content_min_y, y)
+                content_max_y = max(content_max_y, y)
             else:
-                # Anti-aliasing edges
-                if r > b + 20 and r > g + 20:
-                    pixels[x, y] = (239, 68, 68, max(0, min(255, 255 - int((g+b)/2))))
+                if r > 230 and g > 230 and b > 230:
+                    pixels[x, y] = (255, 255, 255, 0)
+                elif r < 120 and g < 120 and b < 120:
+                    pixels[x, y] = (255, 255, 255, a)
+                    content_min_x = min(content_min_x, x)
+                    content_max_x = max(content_max_x, x)
+                    content_min_y = min(content_min_y, y)
+                    content_max_y = max(content_max_y, y)
                 else:
                     brightness = (r + g + b) // 3
-                    if brightness < 240:
+                    if brightness < 230:
                         alpha = max(0, min(255, 255 - brightness))
                         pixels[x, y] = (255, 255, 255, alpha)
-                        min_x, max_x = min(min_x, x), max(max_x, x)
-                        min_y, max_y = min(min_y, y), max(max_y, y)
+                        content_min_x = min(content_min_x, x)
+                        content_max_x = max(content_max_x, x)
+                        content_min_y = min(content_min_y, y)
+                        content_max_y = max(content_max_y, y)
                     else:
                         pixels[x, y] = (255, 255, 255, 0)
                         
-    min_x = max(0, min_x - 10)
-    max_x = min(width, max_x + 10)
-    min_y = max(0, min_y - 10)
-    max_y = min(height, max_y + 10)
+    content_min_x = max(0, content_min_x - 10)
+    content_max_x = min(width, content_max_x + 10)
+    content_min_y = max(0, content_min_y - 10)
+    content_max_y = min(height, content_max_y + 10)
     
-    if max_x > min_x and max_y > min_y:
-        cropped = new_img.crop((min_x, min_y, max_x, max_y))
+    if content_max_x > content_min_x and content_max_y > content_min_y:
+        cropped = new_img.crop((content_min_x, content_min_y, content_max_x, content_max_y))
         cropped.save('src/assets/logo-white.png', 'PNG')
     else:
         new_img.save('src/assets/logo-white.png', 'PNG')
